624427902c3d709232083fbc1bcac2475d62a039
Otter <otc@otterthecat.com>
2013-11-24 13:59:10 -0600

    some refactoring to lib/Db.js

--- test/specs/DbTest.js
+++ test/specs/DbTest.js
@@ -14,7 +14,6 @@ var Db = require('../../lib/Db');
 
 describe('Db', function(){
 
-
     describe("#makeConnection()", function(done){
 
         it("should return a connection object", function(done){
@@ -49,11 +48,48 @@ describe('Db', function(){
         });
     });
 
-    describe("#saveToCollection()", function(){
+    describe("#getCollections()", function(){
 
-        it("should save a document to specified collection", function(done){
+        it("should return an array of all collections", function(done){
 
-            Db.saveToCollection(_doc, 'mocha', function(err, result){
+            Db.getCollections(function(error, array){
+
+                array.should.be.a('array');
+                done();
+            });
+        });
+    });
+
+
+    describe("#use()", function(){
+
+        it("should return requested collection if no callback is passed", function(){
+
+            Db.use('mocha').should.be.an('object');
+        });
+
+
+        it("should pass error/collection to callback function", function(done){
+
+            Db.use('mocha', function(error, collection){
+
+                collection.should.be.an('object');
+                done();
+            });
+        });
+
+        it("should return false if passed callback is not a function", function(){
+
+            Db.use('mocha', 'string').should.equal(false);
+        });
+    });
+
+
+    describe("#save()", function(){
+
+        it("should be chained after #use(), save passed document and return error/result", function(done){
+
+            Db.use('mocha').save(_doc, function(error, result){
 
                 result.should.be.an('object');
                 done();
