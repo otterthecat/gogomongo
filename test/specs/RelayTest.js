e84dced96ec68a0dd1d93bff8b2b3a35deeab635
Otter <otc@otterthecat.com>
2013-11-24 20:17:32 -0600

    Adding lib/Relay.js to help manage events

---
+++ test/specs/RelayTest.js
@@ -0,0 +1,62 @@
+// assertion library
+// /////////////////////////////////////////////////////////
+var chai = require('chai').should();
+
+
+// mock objects
+// /////////////////////////////////////////////////////////
+var EE = require('events').EventEmitter;
+var mockObj = function(){ this.name = "mock"};
+
+// modules to test
+// /////////////////////////////////////////////////////////
+var Relay = require('../../lib/Relay');
+
+describe('Relay', function(){
+
+    var relay = new Relay();
+
+    it("should inhert from EventEmitter", function(){
+
+        relay.should.be.instanceOf(EE);
+    });
+
+
+    describe("#extend()", function(){
+
+        relay.extend(mockObj);
+
+        it("should take passed object and have it inherit Relay", function(){
+
+            var mo = new mockObj();
+            mo.should.be.instanceOf(Relay);
+        });
+    });
+
+    describe("#addRelay()", function(){
+
+        var mo = new mockObj();
+        mo.on('mocha:test', function(){
+            console.log("FOOBAR");
+        });
+        relay.on('blah', function(){
+
+            console.log("SHABANG");
+        });
+
+        mo.run('blah');
+        relay.addRelay(mo);
+        it("should add passed object to internal relays array", function(){
+
+            relay.relays.should.include(mo);
+        });
+    });
+
+    describe("#run()", function(){
+
+        it("should loop through relays and trigger event on each object", function(){
+
+            relay.run('mocha:test');
+        });
+    });
+});
